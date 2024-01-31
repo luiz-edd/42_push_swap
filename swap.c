a62152b9f4821ff2b25a3193f94342f066902ac2
Luiz Eduardo D <leduard2@student.42sp.org.br>
2024-01-31 19:42:21 -0300

    creating swap function

--- swap.c
+++ swap.c
@@ -1,34 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   swap.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: leduard2 <leduard2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/26 12:23:02 by leduard2          #+#    #+#             */
-/*   Updated: 2024/01/31 19:03:51 by leduard2         ###   ########.fr       */
+/*   Created: 2024/01/31 19:12:09 by leduard2          #+#    #+#             */
+/*   Updated: 2024/01/31 19:40:39 by leduard2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void push_swap(t_stack *stack, int stack_size)
+void	swap(t_stack **head)
 {
-	
-}
+	t_stack *a;
+	t_stack *b;
+	t_stack *tmp;
+	a = *head;
+	b = a->next;
+	tmp = b->next;
 
-int	main(int argc, char **argv)
-{
-	t_stack *stack_a;
-	t_stack *stack_b;
-	int len;
-	
-	
-	stack_a = create_stack(argc, argv);
-	stack_b = NULL;
-	len = get_stack_len(stack_a);
-
-	assign_index(stack_a, len);
-	print_stack(stack_a, "stack_a");
-	
+	*head = (*head)->next;
+	(*head)->next = a;
+	a->next = tmp;
 }
